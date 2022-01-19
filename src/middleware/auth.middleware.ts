e252d404a780acb6cdcbf952575ea748536829e9
esmattoha <mondaldipu904@gmail.com>
2022-01-19 20:54:21 +0530

    Auth middleware added

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,61 @@
+import { NextFunction, Request, Response } from "express";
+import { User, UserDocument } from "./../model/user.model";
+import catchAsync from "./../utils/catchAsync.utils";
+import jwtHelper from "./../utils/jwtHelper.utils";
+
+// Login required middleware
+const checkLogin = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const token = jwtHelper.extractToken(req) as string;
+
+    if (!token) {
+      return res.status(401).json({
+        status: "fail",
+        message: "Unauthorized request 💀",
+      });
+    }
+
+    const decoded = jwtHelper.verifyToken(token);
+    const userId: string = Object(decoded).id;
+
+    if (!userId) {
+      return res.status(401).json({
+        status: "fail",
+        message: "Unauthorized Request",
+      });
+    }
+
+    const user = (await User.findById(userId)) as UserDocument;
+
+    if (user.verification_token) {
+      return res.status(401).json({
+        status: "fail",
+        message: "Please verify your email",
+      });
+    }
+
+    if (!user) {
+      return res.status(401).json({
+        status: "fail",
+        message: "Unauthorized Request",
+      });
+    }
+
+    res.locals.user = user;
+
+    next();
+  }
+);
+
+// const checkAdmin = catchAsync(async (req, res, next) => {
+//   if (req.user.role !== 'admin') {
+//     return res.status(401).json({
+//       status: 'fail',
+//       message: 'Unauthorized Request',
+//     });
+//   }
+
+//   next();
+// });
+
+export default { checkLogin };
