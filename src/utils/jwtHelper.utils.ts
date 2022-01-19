a3e6785de2a5bcd103ba81b501a3fcd84c0e631b
esmattoha <mondaldipu904@gmail.com>
2022-01-19 16:05:22 +0530

    jwtHelper Added

---
+++ src/utils/jwtHelper.utils.ts
@@ -0,0 +1,24 @@
+import { Request } from "express";
+import jwt from "jsonwebtoken";
+
+const extractToken = (req: Request) => {
+  let token = req.header("authorization") as string;
+
+  if (!token) {
+    return false;
+  }
+
+  token = token.split(" ")[1];
+  return token;
+};
+
+const verifyToken = (token: string) =>
+  jwt.verify(token, Object(process.env).JWT_SECRET_KEY);
+
+const signToken = (id: string, rememberme: boolean) => {
+  console.log(Object(process.env).JWT_SECRET_KEY);
+  jwt.sign({ id }, Object(process.env).JWT_SECRET_KEY, {
+    expiresIn: rememberme ? "7d" : "1d",
+  });
+};
+export default { extractToken, verifyToken, signToken };
