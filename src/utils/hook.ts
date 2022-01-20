ea193a6484cf1371dc3aff5885fe696dde5cd3c5
esmattoha <mondaldipu904@gmail.com>
2022-01-20 12:46:58 +0530

    Hook Feature added

---
+++ src/utils/hook.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+export default async () => {
+  const url = `https://api.vercel.com/v1/integrations/deploy/prj_NINi8Y375EgydH09CR4lzcRZ0RWj/jVxJuyzSvB`;
+  try {
+    await axios.post(url);
+    return true;
+  } catch (err) {
+    console.log(err);
+  }
+};
