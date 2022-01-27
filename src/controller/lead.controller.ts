effcc752fa0aaa9841ab7f611fc1d28abca1a676
esmattoha <mondaldipu904@gmail.com>
2022-01-27 19:02:11 +0530

    Feat : Service added on lead route

--- src/controller/lead.controller.ts
+++ src/controller/lead.controller.ts
@@ -1,71 +1,42 @@
 // import { validationResult } from 'express-validator';
 import { NextFunction, Request, Response } from "express";
-import Lead from "./../model/lead.model";
-import APIFeatures from "./../utils/apiFeture.utils";
+import { CreateLead, FeatchAllLead } from "../service/lead.service";
 import catchAsync from "./../utils/catchAsync.utils";
 import emailHolper from "./../utils/emailHandler.utils";
 
-type leadType = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => void | object;
+const index = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const leads = await FeatchAllLead(req.query);
 
-const index: leadType = catchAsync(async (req, res, next) => {
-  console.log(Lead.find());
-  const features = new APIFeatures(Lead.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
+    res.status(200).json({
+      status: "success",
+      result: leads.length,
+      data: leads,
+    });
+  }
+);
 
-  const leads = await features.query;
-
-  res.status(200).json({
-    status: "success",
-    result: leads.length,
-    data: leads,
-  });
-});
-
-const store: leadType = catchAsync(async (req, res, next) => {
-  // const errors = validationResult(req);
-  // if (!errors.isEmpty()) {
-  //   return res.status(422).json({
-  //     error: {
-  //       ...errors,
-  //     },
-  //   });
-  // }
-
-  const leadData = req.body;
-
-  const lead = await Lead.create({
-    name: leadData.name as string,
-    email: leadData.email as string,
-    message: leadData.message as string,
-    meta: {
-      purpose: leadData.purpose as string,
-    },
-  });
-
-  if (lead) {
-    await emailHolper.sendEmail({
-      email: "hello@foxflue.com",
-      subject: "New Lead",
-      body: `
+const store = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const lead = await CreateLead(req.body);
+    if (lead) {
+      await emailHolper.sendEmail({
+        email: "hello@foxflue.com",
+        subject: "New Lead",
+        body: `
         Name: ${lead.name}
         Email: ${lead.email}
         Message: ${lead.message}
         Purpose: ${Object(lead.meta).purpose}
       `,
+      });
+    }
+
+    res.status(201).json({
+      status: "success",
     });
   }
-
-  res.status(201).json({
-    status: "success",
-  });
-});
+);
 
 export default {
   index,
---
+++ src/service/lead.service.ts
@@ -0,0 +1,32 @@
+import { DocumentDefinition } from "mongoose";
+import APIFeatures from "../utils/apiFeture.utils";
+import Lead, { LeadDocument } from "./../model/lead.model";
+
+export async function CreateLead(input: DocumentDefinition<LeadDocument>) {
+  try {
+    return await Lead.create({
+      name: input.name,
+      email: input.email,
+      message: input.message,
+      meta: {
+        purpose: input.purpose,
+      },
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function FeatchAllLead(query: object) {
+  try {
+    const features = new APIFeatures(Lead.find(), query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    return await features.query;
+  } catch (error) {
+    throw error;
+  }
+}
