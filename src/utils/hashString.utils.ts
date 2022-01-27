c1ad0ce0d026d737c5ae11975f2dab92caeb055b
esmattoha <mondaldipu904@gmail.com>
2022-01-27 18:46:40 +0530

    change a bit

--- src/utils/hashString.utils.ts
+++ src/utils/hashString.utils.ts
@@ -2,5 +2,5 @@ import crypto from "crypto";
 
 export const hashString = () => crypto.randomBytes(32).toString("hex");
 
-export const encryptedRandomString = (verificationToken: string) =>
-  crypto.createHash("sha256").update(verificationToken).digest("hex");
+export const encryptedRandomString = (text: string) =>
+  crypto.createHash("sha256").update(text).digest("hex");
