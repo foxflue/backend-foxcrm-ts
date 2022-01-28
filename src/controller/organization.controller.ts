9c5ed62ffe540e6bc98506a06dc1483d7bf27bfc
esmattoha <mondaldipu904@gmail.com>
2022-01-28 19:37:08 +0530

    Feat : organization controller added

---
+++ src/controller/organization.controller.ts
@@ -0,0 +1,93 @@
+import { NextFunction, Request, Response } from "express";
+import { orgRegisteredEmail } from "../emailContent/org.registered.emailContent";
+import catchAsync from "../utils/catchAsync.utils";
+import {
+  CreateOrg,
+  EmailVerification,
+  FetchAllOrg,
+  FetchOrg,
+  UpdateOrg,
+} from "./../service/organization.service";
+import emailHelper from "./../utils/emailHandler.utils";
+
+const store = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { organization, verificationToken } = await CreateOrg(req.body);
+
+    // Send Greetings Email
+    await emailHelper.sendEmail({
+      email: organization.email,
+      subject: "Welcome to Foxflue",
+      body: await orgRegisteredEmail(organization.title, verificationToken),
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Please verify your email.",
+    });
+  }
+);
+
+const orgEmailVerify = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    await EmailVerification(req.params.token);
+
+    res.status(200).json({
+      status: "success",
+      message: "Email verified.",
+    });
+  }
+);
+
+const index = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const organizations = await FetchAllOrg(req.query);
+
+    res.status(200).json({
+      status: "success",
+      results: organizations.length,
+      data: organizations,
+    });
+  }
+);
+
+const show = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const organization = await FetchOrg(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: organization,
+    });
+  }
+);
+
+const update = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const organization = await UpdateOrg(req.params.id, req.body);
+
+    res.status(200).json({
+      status: "success",
+      data: organization,
+    });
+  }
+);
+
+const destroy = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    await FetchOrg(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: null,
+    });
+  }
+);
+
+export default {
+  store,
+  index,
+  show,
+  update,
+  destroy,
+};
