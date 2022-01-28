a1820e9f2e7f76027d1b87edcb7ecf198d4038fa
esmattoha <mondaldipu904@gmail.com>
2022-01-28 13:47:02 +0530

    Feat : Organization model added

---
+++ src/model/organization.model.ts
@@ -0,0 +1,49 @@
+import { Document, model, Schema } from "mongoose";
+
+export interface OrganizationDocument extends Document {
+  _id: string;
+  email: string;
+  phone: string;
+  about: string;
+  image: string;
+  admin: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const organizationSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    admin: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Organization = model<OrganizationDocument>(
+  "Organization",
+  organizationSchema
+);
+
+export default Organization;
