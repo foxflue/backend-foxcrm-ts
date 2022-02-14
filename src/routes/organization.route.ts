721497091d740e0f93723804a13cf748424f0ddd
esmattoha <mondaldipu904@gmail.com>
2022-02-14 21:52:36 +0530

    create org updated

--- src/routes/organization.route.ts
+++ src/routes/organization.route.ts
@@ -1,21 +1,13 @@
 import { Router } from "express";
 import organizationController from "../controller/organization.controller";
-import { organizationSchema } from "../schema/organization.schema";
 import authMiddleware from "./../middleware/auth.middleware";
-import { validate } from "./../middleware/validate.middleware";
 
 const router = Router();
 
 router.post(
   "/org-create",
   [authMiddleware.checkLogin, authMiddleware.checkAdmin],
-  validate(organizationSchema),
   organizationController.store
-);
-router.post(
-  "/org-email-verify/:token",
-  [authMiddleware.checkLogin, authMiddleware.checkAdmin],
-  organizationController.orgEmailVerify
 );
 
 router.get(
